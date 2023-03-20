2e9a79819fccfc119739dddafa26e3c26ed901a2
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-03-20 14:21:48 +0530

    Task of 20.03.2023

---
+++ FunOverloadingOverridingAbstractInterface/funOverriding.java
@@ -0,0 +1,14 @@
+package Occurances;
+
+public class funOverriding {
+	 void run() {System.out.println("Vehicle is running");}  
+		}  
+		//Creating a child class  
+		class Bike extends funOverriding{  
+		  public static void main(String args[]){  
+		  //creating an instance of child class  
+		  Bike obj = new Bike();  
+		  //calling the method with child class instance  
+		  obj.run();  
+		  }  
+		}  
---
+++ StreamMap.java
@@ -0,0 +1,18 @@
+package Occurances;
+import java.util.*;
+import java.util.stream.Collectors;
+  
+public class StreamMap {
+
+public static void main(String[] args) {
+List<Integer>list=new ArrayList<>();
+list.add(10);
+list.add(20);
+list.add(30);
+list.add(40);
+List<Integer> answer = list.stream().map(X->X*X).
+collect(Collectors.toList());
+System.out.println(answer);	
+	}
+
+}
