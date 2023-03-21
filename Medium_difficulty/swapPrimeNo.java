9a8a148abf3c3134fcf9cb8be319472ae5e00e1c
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-03-21 20:56:53 +0530

    Medium defficulty program

--- Medium_difficulty/swapPrimeNo.java
+++ Medium_difficulty/swapPrimeNo.java
@@ -1,35 +1,23 @@
-import java.util.Scanner;
 public class swapPrimeNo {
     public static void main(String[] args) {
-        int n;
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter Array Size:");
-        n=sc.nextInt();
-        int[] a = new int[n];
-        System.out.println("Enter Array elements: ");
-        for(int i=0; i<n; i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        for (int i = 0; i <= n; i++) {
-            int b;
-            b = a[a.length - 1];
-            if (b % 2 != 0) {
+        int[] someArray = {1, 2, 3, 7};
+        SwapPrimeNumber(someArray);
+    }
+
+    public static void SwapPrimeNumber(int[] X) {
+        for (int i = 0; i <= X.length; i++) {
+            int lastElement;
+            lastElement = X[X.length - 1];
+            if (lastElement % 2 != 0) {
                 int temp = 0;
-                temp = a[i];
-                a[i] = a[a.length - 1];
-                a[a.length - 1] = temp;
-
+                temp = X[i];
+                X[i] = X[X.length - 1];
+                X[X.length - 1] = temp;
             }
+            for (i = 0; i < X.length; i++)
+                System.out.print(X[i] + " ");
         }
-            for(int i=0;i<n;i++)
-                System.out.print(a[i]+" ");
-
-
-        //System.out.print(b);
-
     }
 }
-
 
 
--- Medium_difficulty/taxSalary.java
+++ Medium_difficulty/taxSalary.java
@@ -1,30 +1,25 @@
 import java.util.Scanner;
 public class taxSalary {
-    public static double taxSalaryCount(double salary)
+    public static void taxSalaryCount(double salary)
  {
- double incomeTaxAmt = 0;
- double TaxPerc = 0;
- if (salary <= 500000)
- TaxPerc = 0;
- else if (salary > 500000 && salary <= 700000)
- TaxPerc = 10;
- else if (salary > 700000 && salary <= 1000000)
- TaxPerc = 15;
- else if (salary > 1000000 && salary <= 1500000)
- TaxPerc = 20;
- else if (salary > 1500000)
- TaxPerc = 30;
- incomeTaxAmt = salary*TaxPerc/100;
- return incomeTaxAmt;
- }
+             double tax=0;
 
+             if(salary<=500000)
+                 tax=0;
+             else if (salary > 500000 && salary <= 700000)
+                 tax=0.1*(salary-500000);
+             else if(salary > 700000 && salary <= 1000000)
+                 tax=(0.15*(salary-700000))+(0.1*200000);
+            else if (salary > 1000000 && salary <= 1500000)
+                 tax=(0.2*(salary-1000000))+(0.15*300000)+(0.1*200000);
+             else
+                 tax=(0.3*(salary-1500000))+(0.2*500000)+(0.15*300000)+(0.1*200000);
+             System.out.println("Income tax amount is "+tax);
+         }
     public static void main(String[] args) {
 
-        Scanner sal = new Scanner(System.in);
-        System.out.print("Enter Employee salary");
-        double empSalaryAmt = sal.nextDouble();
-       double incomeTaxAmt= taxSalaryCount(empSalaryAmt);
-       System.out.println("Employee income Tax amount = Rs. "+incomeTaxAmt);
+      taxSalaryCount(2000000);
+       //System.out.println("Employee income Tax amount = Rs. "+incomeTaxAmt);
 
     }
 }
