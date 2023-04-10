f7f37c905ff03b8aa33dadb1ecd036ff883e0a6a
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-04-10 21:32:43 +0530

    Done Changes

--- swapPrimeNo/SwapPrimeNo.java
+++ swapPrimeNo/SwapPrimeNo.java
@@ -5,40 +5,46 @@ import java.util.*;
 public class SwapPrimeNo {
     private int temporary;
     private int lastNumber;
-   private boolean flag = false;
+    private boolean flag = false;
 
     /*
     Q3. Given an array of ints, swap the first and last elements in the array. Return the modified array.
      If the last array value is prime no.
      */
     public static void main(String[] args) {
-        SwapPrimeNo swapObject = new SwapPrimeNo();
         int[] inputArray = {1, 2, 3, 7};
         int[] inputArraySecond = {1, 2, 3};
         int[] inputArrayThird = {1, 6, 7, 9, 8};
-        swapObject.swapPrimeNumber(inputArray);
-        swapObject.swapPrimeNumber(inputArraySecond);
-        swapObject.swapPrimeNumber(inputArrayThird);
+        int lastNumber = inputArray[inputArray.length - 1];
+        swapPrimeNumber(lastNumber, 2);
+        printResult(inputArray, lastNumber);
+        lastNumber = inputArraySecond[inputArraySecond.length - 1];
+        swapPrimeNumber(lastNumber, 2);
+        printResult(inputArraySecond, lastNumber);
+        lastNumber = inputArrayThird[inputArrayThird.length - 1];
+        swapPrimeNumber(lastNumber, 2);
+        printResult(inputArrayThird, lastNumber);
     }
 
-    public void swapPrimeNumber(int inputArray[]) {
-        lastNumber = inputArray[inputArray.length - 1];
-        int midNumber = lastNumber / 2;
-        for (int counter = 2; counter <= midNumber; counter++) {
-            if (lastNumber % counter == 0) {
-                flag = true;
-                break;
-            }
+    public static boolean swapPrimeNumber(int number, int counter) {
+        boolean result = false;
+        if (number % counter == 0) {
+            return false;
         }
-        if (flag == true || lastNumber == 0 || lastNumber == 1) {
-            System.out.println("Last number of array is : " + lastNumber + " is not prime so not swaped");
-            System.out.println("new array is : " + Arrays.toString(inputArray));
-        }
-        else {
+        if (counter * counter > number)
+            return true;
+        return swapPrimeNumber(number, counter + 1);
+    }
+
+    public static void printResult(int[] inputArray, int lastNumber) {
+        if (swapPrimeNumber(lastNumber, 2) || lastNumber == 0 || lastNumber == 1) {
             System.out.println("Array last element is : " + lastNumber + " And Number is prime");
-            temporary = inputArray[0];
+            int temporary = inputArray[0];
             inputArray[0] = lastNumber;
             inputArray[inputArray.length - 1] = temporary;
+            System.out.println("new array is : " + Arrays.toString(inputArray));
+        } else {
+            System.out.println("Last number of array is : " + lastNumber + " is not prime so not swapped");
             System.out.println("new array is : " + Arrays.toString(inputArray));
         }
     }
