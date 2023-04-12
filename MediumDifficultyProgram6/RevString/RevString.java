dc90532e169519f145bf82acc0184fea6687df1c
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-04-12 18:56:37 +0530

    Changes done

--- MediumDifficultyProgram6/RevString/RevString.java
+++ MediumDifficultyProgram6/RevString/RevString.java
@@ -5,19 +5,18 @@ public class RevString {
     Q6. Write a program to reverse the string without changing the position of special character.
  */
     public static void main(String[] args) {
-        String result = Reverse("Ro@hitshetty@");
+        String result = Reverse("Ro@hitshetty");
         System.out.println(result);
     }
 
     static String Reverse(String inputString) {
         String result = "";
-        String specialCharatcterList = "@,#,$,<,>,?,/,^,&,*,(,),{,},[,],:,.";
         for (int counter = 0; counter < inputString.length(); counter++) {
             char reverseChar = inputString.charAt(inputString.length() - (counter + 1));
-            if (specialCharatcterList.contains(Character.toString(reverseChar)))
+            if (!Character.isLetterOrDigit(reverseChar))
                 continue;
-            String currentCharacter = Character.toString(inputString.charAt(counter));
-            if (specialCharatcterList.contains(currentCharacter)) {
+            char currentCharacter = inputString.charAt(counter);
+            if (!Character.isLetterOrDigit(currentCharacter)) {
                 reverseChar = inputString.charAt(counter);
             }
             result += reverseChar;
