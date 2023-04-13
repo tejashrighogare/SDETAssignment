7c63c12a537faa6f2a85c6c08eead6ff6042c7bc
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-04-13 13:55:51 +0530

    Done changes as per comment

--- MediumDifficultyProgram6/RevString/RevString.java
+++ MediumDifficultyProgram6/RevString/RevString.java
@@ -5,23 +5,31 @@ public class RevString {
     Q6. Write a program to reverse the string without changing the position of special character.
  */
     public static void main(String[] args) {
-        String result = reverseString("Ro@hitshetty");
+        String result = reverseString("Ro@hitshetty@");
+        System.out.println(result);
+        result = reverseString("Ro@hitshetty");
         System.out.println(result);
     }
 
     static String reverseString(String inputString) {
         String result = "";
+        int reverseCharacterCounter = 0;
         for (int counter = 0; counter < inputString.length(); counter++) {
-            char reverseChar = inputString.charAt(inputString.length() - (counter + 1));
+            if (!Character.isLetterOrDigit(inputString.charAt(inputString.length() - 1)) && counter == inputString.length() - 1)
+                reverseCharacterCounter--;
+            char reverseChar = inputString.charAt(inputString.length() - (reverseCharacterCounter + 1));
             if (!Character.isLetterOrDigit(reverseChar)) {
-                continue;
+                reverseCharacterCounter++;
+                reverseChar = inputString.charAt(inputString.length() - (reverseCharacterCounter + 1));
             }
             char currentCharacter = inputString.charAt(counter);
-            String stringValue=Character.toString(reverseChar);
+            String stringValue = Character.toString(reverseChar);
             if (!Character.isLetterOrDigit(currentCharacter)) {
-                stringValue = inputString.charAt(counter)+ stringValue;
+                stringValue = Character.toString(currentCharacter);
+                reverseCharacterCounter--;
             }
             result += stringValue;
+            reverseCharacterCounter++;
         }
         return result;
     }
