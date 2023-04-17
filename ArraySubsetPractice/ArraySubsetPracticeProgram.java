62949bc41017c021060a44edd3fb386c1a5845fe
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-04-17 13:18:23 +0530

    Java coding test Q.4

---
+++ ArraySubsetPractice/ArraySubsetPracticeProgram.java
@@ -0,0 +1,27 @@
+package ArraySubsetPractice;
+
+import java.util.*;
+
+public class ArraySubsetPracticeProgram {
+    public static void main(String[] args) {
+        int[] arrayInput = {1, 2, 3};
+        List<List<Integer>> result;
+        result = arraySubsets(arrayInput);
+        System.out.println(result);
+    }
+
+    public static List<List<Integer>> arraySubsets(int[] arrayInput) {
+        List<List<Integer>> arraylistObject = new ArrayList<>();
+        arraylistObject.add(new ArrayList<>());
+        for (int inputArrayElement : arrayInput) {
+            int arraylistSize = arraylistObject.size();
+            for (int count = 0; count < arraylistSize; count++) {
+                List<Integer> internalElements = new ArrayList<>(arraylistObject.get(count));
+                internalElements.add(inputArrayElement);
+
+                arraylistObject.add(internalElements);
+            }
+        }
+        return arraylistObject;
+    }
+}
