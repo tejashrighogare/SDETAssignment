970077caf992a094fda5217d9084d616e98aca27
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-04-26 21:55:21 +0530

    Added coding exercise program

---
+++ armstrongNumber/armstrongNumberProgram.java
@@ -0,0 +1,27 @@
+package armstrongNumber;
+
+public class armstrongNumberProgram {
+    public static void isArmstrong(int number) {
+        int originalNumber, remainder, result = 0;
+
+        originalNumber = number;
+
+        while (originalNumber != 0) {
+            remainder = originalNumber % 10;
+            result += remainder * remainder * remainder;
+            originalNumber /= 10;
+        }
+
+        if (result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+
+    public static void main(String args[]) {
+        int number = 407;
+        isArmstrong(number);
+        number = 131;
+        isArmstrong(number);
+    }
+}
