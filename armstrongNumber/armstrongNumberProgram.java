ecca4401f52f387a29e3e72ca9ef6177d9cf63e1
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-04-26 22:05:07 +0530

    Done changes

--- armstrongNumber/armstrongNumberProgram.java
+++ armstrongNumber/armstrongNumberProgram.java
@@ -3,15 +3,12 @@ package armstrongNumber;
 public class armstrongNumberProgram {
     public static void isArmstrong(int number) {
         int originalNumber, remainder, result = 0;
-
         originalNumber = number;
-
         while (originalNumber != 0) {
             remainder = originalNumber % 10;
             result += remainder * remainder * remainder;
             originalNumber /= 10;
         }
-
         if (result == number)
             System.out.println(number + " is an Armstrong number.");
         else
