d557358fbc1b162bb03f118bc7e1e7bf2246b353
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-05-12 13:16:53 +0530

    Added Java Coding Program 8

---
+++ lambdaInArrayList/LambdaInArraylistProgram.java
@@ -0,0 +1,28 @@
+package lambdaInArrayList;
+
+import java.util.*;
+
+public class LambdaInArraylistProgram {
+    public static void main(String[] args) {
+        ArrayList<String> arrayListObject = new ArrayList<String>();
+        arrayListObject.add("Anil");
+        arrayListObject.add("Vijay");
+        arrayListObject.add("Ajay");
+        arrayListObject.add("Ankit");
+        arrayListObject.add("Shivam");
+        System.out.println("An initial list of elements: " + arrayListObject);
+        arrayListObject.remove("Vijay");
+        System.out.println("After invoking remove(object) method: " + arrayListObject);
+        arrayListObject.remove(0);
+        System.out.println("After invoking remove(index) method: " + arrayListObject);
+        ArrayList<String> newArrayListObject = new ArrayList<String>();
+        newArrayListObject.add("Ravi");
+        newArrayListObject.add("Hanumant");
+        arrayListObject.addAll(newArrayListObject);
+        System.out.println("Updated list : " + arrayListObject);
+        arrayListObject.removeAll(newArrayListObject);
+        System.out.println("After invoking removeAll() method: " + arrayListObject);
+        arrayListObject.removeIf(stringElement -> stringElement.contains("Ajay"));
+        System.out.println("After invoking removeIf() method using lambda function: " + arrayListObject);
+    }
+}
