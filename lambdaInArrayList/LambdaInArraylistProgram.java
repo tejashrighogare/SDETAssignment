1b9d5a14f112e7985729a3adaa0c295f36f4cee1
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-05-19 12:27:52 +0530

    Code is updated

--- lambdaInArrayList/LambdaInArraylistProgram.java
+++ lambdaInArrayList/LambdaInArraylistProgram.java
@@ -5,25 +5,19 @@ import java.util.*;
 public class LambdaInArraylistProgram {
     //Java ArrayList Program to remove elements of Arraylist using lambda function.
     public static void main(String[] args) {
-        ArrayList<String> arrayListObject = new ArrayList<String>();
-        arrayListObject.add("Anil");
-        arrayListObject.add("Vijay");
-        arrayListObject.add("Ajay");
-        arrayListObject.add("Ankit");
-        arrayListObject.add("Shivam");
-        System.out.println("An initial list of elements: " + arrayListObject);
-        arrayListObject.remove("Vijay");
-        System.out.println("After invoking remove(object) method: " + arrayListObject);
-        arrayListObject.remove(0);
-        System.out.println("After invoking remove(index) method: " + arrayListObject);
-        ArrayList<String> newArrayListObject = new ArrayList<String>();
-        newArrayListObject.add("Ravi");
-        newArrayListObject.add("Hanumant");
-        arrayListObject.addAll(newArrayListObject);
-        System.out.println("Updated list : " + arrayListObject);
-        arrayListObject.removeAll(newArrayListObject);
-        System.out.println("After invoking removeAll() method: " + arrayListObject);
-        arrayListObject.removeIf(stringElement -> stringElement.contains("Ajay"));
-        System.out.println("After invoking removeIf() method using lambda function: " + arrayListObject);
+        List<String> listObject = Arrays.asList("Anil", "Vijay", "Ajay", "Ankt", "Shivam");
+        ArrayList<String> arrayList = new ArrayList<String>(listObject);
+        System.out.println("An initial list of elements: " + arrayList);
+        arrayList.remove("Vijay");
+        System.out.println("After invoking remove(object) method: " + arrayList);
+        arrayList.remove(0);
+        System.out.println("After invoking remove(index) method: " + arrayList);
+        List<String> newListObject = Arrays.asList("Ravi", "Hanumant");
+        ArrayList<String> newArrayList = new ArrayList<String>(newListObject);
+        System.out.println("Updated list : " + newArrayList);
+        arrayList.removeAll(newArrayList);
+        System.out.println("After invoking removeAll() method: " + arrayList);
+        arrayList.removeIf(stringElement -> stringElement.contains("Ajay"));
+        System.out.println("After invoking removeIf() method using lambda function: " + arrayList);
     }
 }
