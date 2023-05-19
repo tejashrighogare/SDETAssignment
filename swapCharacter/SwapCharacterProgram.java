4feb1fe3b2ae77330e388a483980a8ae49bb0aa6
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-05-19 12:33:15 +0530

    Changed variable name

--- swapCharacter/SwapCharacterProgram.java
+++ swapCharacter/SwapCharacterProgram.java
@@ -3,19 +3,19 @@ package swapCharacter;
 public class SwapCharacterProgram {
     public static void main(String[] args) {
         String inputString = "ABCDEFGH";
-        int swapNumber = 4;
-        int count = 3;
-        swapCharacters(inputString, swapNumber, count);
+        int swapCounter = 4;
+        int characterCounter = 3;
+        swapCharacters(inputString, swapCounter, characterCounter);
     }
 
-    public static void swapCharacters(String inputString, int swapNumber, int count) {
+    public static void swapCharacters(String inputString, int swapCounter, int characterCounter) {
         int stringLength = inputString.length();
-        count = count % stringLength;
+        characterCounter = characterCounter % stringLength;
         char[] character = inputString.toCharArray();
-        for (int index = 0; index < swapNumber; index++) {
+        for (int index = 0; index < swapCounter; index++) {
             char temporary = character[index];
-            character[index] = character[(index + count) % stringLength];
-            character[(index + count) % stringLength] = temporary;
+            character[index] = character[(index + characterCounter) % stringLength];
+            character[(index + characterCounter) % stringLength] = temporary;
             System.out.println("after " + index + " index swap " + String.valueOf(character));
         }
     }
