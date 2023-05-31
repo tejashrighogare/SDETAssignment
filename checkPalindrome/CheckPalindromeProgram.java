ce7e42d5ef2ec4c3b81f4eca03d736b532be96c7
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-05-31 14:34:39 +0530

    done changes

--- checkPalindrome/CheckPalindromeProgram.java
+++ checkPalindrome/CheckPalindromeProgram.java
@@ -5,8 +5,9 @@ package checkPalindrome;
  * Java program to check if the number is palindrome or not.
  */
 public class CheckPalindromeProgram {
+    private static int inputNumber = 121, temporaryNumber, finalNumber;
+
     public static void main(String[] args) {
-        int inputNumber = 121, temporaryNumber, finalNumber;
         if (inputNumber < 0) {
             System.out.println("Number is not pallindrome");
             return;
@@ -23,7 +24,6 @@ public class CheckPalindromeProgram {
     private int sum = 0, remainder;
 
     public int palindromeOrNot(int inputNumber) {
-
         if (inputNumber != 0) {
             remainder = inputNumber % 10;  //rem=1,2,1
             sum = (sum * 10) + remainder;   //sum=1,12,121
