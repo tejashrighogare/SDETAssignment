7b3f45896c7623ed6dbac74789ef65e2ef076032
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-06-02 13:20:27 +0530

    Done changes

--- fibonicciSeries/FibonicciSeriesProgram.java
+++ fibonicciSeries/FibonicciSeriesProgram.java
@@ -1,18 +1,22 @@
 package fibonicciSeries;
 
 public class FibonicciSeriesProgram {
+    private static int firstNumber = 0;
+    private static int secondNumber = 1;
+    private static int nextNumber = 0;
+
     public static void main(String args[]) {
+        System.out.print(firstNumber + " " + secondNumber);
         fibonacciSeries(10);
     }
 
     public static void fibonacciSeries(int count) {
-        int firstNumber = 0, secondNumber = 1, nextNumber, index;
-        System.out.print(firstNumber + " " + secondNumber);
-        for (index = 2; index < count; ++index) {
+        if (count > 0) {
             nextNumber = firstNumber + secondNumber;
             System.out.print(" " + nextNumber);
             firstNumber = secondNumber;
             secondNumber = nextNumber;
+            fibonacciSeries(count - 1);
         }
     }
 }
